7169f6feed4c1b684b4610535db3dd3ca43aabe5
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-07 17:27:37 -0400

    Update backend.tf

--- env/QA/backend.tf
+++ env/QA/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "dhdp-qa-resource-group"
-    storage_account_name = "dhdpqatfstg"
-    container_name       = "tfstate"
-    key                  = "qa.terraform.tfstate"
+    resource_group_name  = "dhdp-lab-resource-group"
+    storage_account_name = "dhdplabsa"             
+    container_name       = "tfstate"               
+    key                  = "qa/terraform.tfstate"  
   }
 }
