92b51499a5371c201cdda186d5113e93d07edc60
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-07 18:50:49 -0400

    Update main.tf

--- terraform_modules/terraform-azure-aks/main.tf
+++ terraform_modules/terraform-azure-aks/main.tf
@@ -6,50 +6,31 @@ resource "azurerm_kubernetes_cluster" "this" {
   kubernetes_version  = var.kubernetes_version
   node_resource_group = var.node_resource_group
 
-default_node_pool {
-  name                = var.default_node_pool.name
-  vm_size             = var.default_node_pool.vm_size
-  enable_auto_scaling = var.default_node_pool.enable_auto_scaling
-  min_count           = var.default_node_pool.min_count
-  max_count           = var.default_node_pool.max_count
-  max_pods            = var.default_node_pool.max_pods
-  os_disk_size_gb     = var.default_node_pool.os_disk_size_gb
-  node_labels         = var.default_node_pool.node_labels
-  vnet_subnet_id      = var.default_node_pool.vnet_subnet_id
-  mode                = "System"               # ✅ this is correct
-  tags                = var.default_node_pool.tags
-}
-
+  default_node_pool {
+    name                = var.default_node_pool.name
+    vm_size             = var.default_node_pool.vm_size
+    enable_auto_scaling = var.default_node_pool.enable_auto_scaling
+    min_count           = var.default_node_pool.min_count
+    max_count           = var.default_node_pool.max_count
+    max_pods            = var.default_node_pool.max_pods
+    os_disk_size_gb     = var.default_node_pool.os_disk_size_gb
+    node_labels         = var.default_node_pool.node_labels
+    vnet_subnet_id      = var.default_node_pool.vnet_subnet_id
+    mode                = "System"  # ✅ valid here only
+    tags                = var.default_node_pool.tags
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
-role_based_access_control_enabled = true
-
+  role_based_access_control_enabled = true
 
   network_profile {
-    network_plugin     = var.network_plugin
-    dns_service_ip     = var.dns_service_ip
-    service_cidr       = var.service_cidr
-    
+    network_plugin = var.network_plugin
+    dns_service_ip = var.dns_service_ip
+    service_cidr   = var.service_cidr
   }
 
   tags = var.tags
-}
-
-resource "azurerm_kubernetes_cluster_node_pool" "user_pools" {
-  for_each = var.user_node_pools
-
-  name                  = each.value.name
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
-  vm_size               = each.value.vm_size
-  node_count            = each.value.node_count
-  max_pods              = each.value.max_pods
-  os_disk_size_gb       = each.value.os_disk_size_gb
-  mode                  = each.value.mode
-  node_labels           = each.value.node_labels
-  vnet_subnet_id        = each.value.vnet_subnet_id
-  orchestrator_version  = var.kubernetes_version
-  tags                  = each.value.tags
 }
