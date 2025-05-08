78f4711e734cbe89c8ed1417f0e41d15707111e5
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-08 00:57:45 -0400

    Update main.tf

--- env/QA/main.tf
+++ env/QA/main.tf
@@ -77,7 +77,14 @@ resource "helm_release" "nginx_ingress" {
 
   depends_on = [module.aks]
 }
-
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.kube_config[0].host
+    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(module.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
+  }
+}
 # Private DNS Zone
 module "private_dns" {
   source                = "../../terraform_modules/terraform-azure-private-dns"
