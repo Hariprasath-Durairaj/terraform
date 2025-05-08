1eb32f9f7e51b6ca6bde89ec73d7d7bcd3d6a030
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-08 01:10:11 -0400

    Update outputs.tf

--- env/QA/outputs.tf
+++ env/QA/outputs.tf
@@ -2,18 +2,3 @@
 
 # You can leave this file empty if outputs are defined in main.tf
 
-output "host" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.host
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_certificate
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_key
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
-}
