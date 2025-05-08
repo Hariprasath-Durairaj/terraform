1248fa9648b3e34b4633c0db52a1bc740b447efe
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-07 22:52:32 -0400

    Update outputs.tf

--- env/QA/outputs.tf
+++ env/QA/outputs.tf
@@ -1,24 +1,3 @@
-output "public_ip_nginx" {
-  value       = module.public_ip_nginx.public_ip_address
-  description = "Public IP address for NGINX Ingress"
-}
+# Remove the duplicate outputs as they are already defined in main.tf
 
-output "key_vault_id" {
-  value       = module.key_vault.key_vault_id
-  description = "Key Vault ID"
-}
-
-output "acr_id" {
-  value       = module.acr.acr_id
-  description = "Azure Container Registry ID"
-}
-
-output "log_analytics_workspace_id" {
-  value       = module.log_analytics.workspace_id
-  description = "Log Analytics Workspace ID"
-}
-
-output "nginx_ingress_controller_id" {
-  value       = helm_release.nginx_ingress.id
-  description = "NGINX Ingress Controller ID"
-}
+# You can leave this file empty if outputs are defined in main.tf
