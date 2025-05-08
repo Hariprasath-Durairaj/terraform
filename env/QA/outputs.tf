89851e38549410104c6f3c70fdc26108fab80a38
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-07 22:48:51 -0400

    Update outputs.tf

--- env/QA/outputs.tf
+++ env/QA/outputs.tf
@@ -1,60 +1,24 @@
-# Output for VNet ID
-output "vnet_id" {
-  value       = module.vnet.vnet_id
-  description = "ID of the Virtual Network"
+output "public_ip_nginx" {
+  value       = module.public_ip_nginx.public_ip_address
+  description = "Public IP address for NGINX Ingress"
 }
 
-# Output for Subnet IDs (if applicable)
-output "subnet_ids" {
-  value       = module.vnet.subnet_ids
-  description = "Map of subnet names to subnet IDs"
+output "key_vault_id" {
+  value       = module.key_vault.key_vault_id
+  description = "Key Vault ID"
 }
 
-# Output for AKS Cluster ID
-output "aks_id" {
-  value       = module.aks.aks_id
-  description = "ID of the AKS Cluster"
+output "acr_id" {
+  value       = module.acr.acr_id
+  description = "Azure Container Registry ID"
 }
 
-# Output for Kubeconfig (Sensitive)
-output "kube_config" {
-  value       = module.aks.kube_config
-  description = "Raw kubeconfig content"
-  sensitive   = true
-}
-
-# Output for Log Analytics Workspace ID
 output "log_analytics_workspace_id" {
   value       = module.log_analytics.workspace_id
-  description = "ID of the Log Analytics Workspace"
+  description = "Log Analytics Workspace ID"
 }
 
-# Output for Backup Vault ID
-output "backup_vault_id" {
-  value       = module.backup.vault_id
-  description = "ID of the Recovery Services Vault"
-}
-
-# If you are not using the Bastion module, remove this output
-output "public_ip_bastion" {
-  value       = module.public_ip_bastion.public_ip_address
-  description = "Public IP address assigned to Bastion Host"
-}
-
-# If you are not using the Application Gateway or WAF, remove these outputs
-output "public_ip_appgw" {
-  value       = module.public_ip_appgw.public_ip_address
-  description = "Public IP address assigned to Application Gateway"
-}
-
-# If you are not using WAF, remove this output
-output "waf_policy_id" {
-  value       = module.waf.waf_policy_id
-  description = "ID of the WAF Policy"
-}
-
-# If you are not using Application Gateway, remove this output
-output "app_gateway_id" {
-  value       = module.app_gateway.app_gateway_id
-  description = "ID of the Application Gateway"
+output "nginx_ingress_controller_id" {
+  value       = helm_release.nginx_ingress.id
+  description = "NGINX Ingress Controller ID"
 }
