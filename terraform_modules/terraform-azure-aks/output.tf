5c7b70f484d02bca6511d0e62fb9f2c020cbb38e
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-08 01:09:52 -0400

    Update output.tf

--- terraform_modules/terraform-azure-aks/output.tf
+++ terraform_modules/terraform-azure-aks/output.tf
@@ -20,3 +20,19 @@ output "kube_config_file" {
     password               = azurerm_kubernetes_cluster.this.kube_config[0].password
   }
 }
+
+output "host" {
+  value = azurerm_kubernetes_cluster.this.kube_config.0.host
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.this.kube_config.0.client_certificate
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.this.kube_config.0.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
+}
