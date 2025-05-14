85a5cae30e9b0f4296ca0fe96a447015b8a1d48b
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-14 13:58:18 -0400

    Update main.tf

--- env/QA/main.tf
+++ env/QA/main.tf
@@ -47,11 +47,12 @@ module "aks" {
 
 # Kubernetes provider using AKS kubeconfig output
 provider "kubernetes" {
-  host                   = module.aks.kube_config.host
-  client_certificate     = base64decode(module.aks.kube_config.client_certificate)
-  client_key             = base64decode(module.aks.kube_config.client_key)
-  cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
 }
+
 
 
 # Create Kubernetes namespaces for each application
