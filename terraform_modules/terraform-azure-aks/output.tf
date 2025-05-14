ee5dc276111df0566be5b6856ab70bf7d529a464
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-14 13:57:29 -0400

    Update output.tf

--- terraform_modules/terraform-azure-aks/output.tf
+++ terraform_modules/terraform-azure-aks/output.tf
@@ -3,14 +3,14 @@ output "aks_id" {
   value       = azurerm_kubernetes_cluster.this.id
 }
 
-output "kube_config" {
-  description = "Kube config raw output"
+output "kube_config_raw" {
+  description = "Raw kubeconfig content (string)"
   value       = azurerm_kubernetes_cluster.this.kube_config_raw
   sensitive   = true
 }
 
 output "kube_config_file" {
-  description = "Kube config block for use with kubectl"
+  description = "Full kubeconfig block for manual use"
   value = {
     host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
     client_certificate     = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
@@ -19,20 +19,28 @@ output "kube_config_file" {
     username               = azurerm_kubernetes_cluster.this.kube_config[0].username
     password               = azurerm_kubernetes_cluster.this.kube_config[0].password
   }
+  sensitive = true
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.host
+  description = "Kubernetes API server host"
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].host
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_certificate
+  description = "Kubernetes client certificate"
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
+  sensitive   = true
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_key
+  description = "Kubernetes client private key"
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].client_key
+  sensitive   = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
+  description = "Kubernetes cluster CA certificate"
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  sensitive   = true
 }
