7379cff069d9ec40c19238c61f5c609193532e3a
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 15:42:00 -0400

    Update main.tf

--- env/QA/main.tf
+++ env/QA/main.tf
@@ -162,7 +162,7 @@ module "acr" {
 resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = module.acr.acr_id
   role_definition_name = "AcrPull"
-  principal_id         = module.aks.kubelet_identity[0].object_id
+  principal_id = module.aks.kubelet_identity_object_id
 }
 
 # Log Analytics Workspace
