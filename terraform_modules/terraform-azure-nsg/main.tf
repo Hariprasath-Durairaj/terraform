aa2de4f7c019ef98504b2771916031b6c217c467
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 15:59:50 -0400

    Update main.tf

--- terraform_modules/terraform-azure-nsg/main.tf
+++ terraform_modules/terraform-azure-nsg/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "this" {
-  name                = var.name
+  name                = var.nsg_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -16,10 +16,11 @@ resource "azurerm_network_security_group" "this" {
       source_address_prefix      = security_rule.value.source_address_prefix
       destination_address_prefix = security_rule.value.destination_address_prefix
     }
-  
+  }
 
   tags = var.tags
-}
+}   # <--- THIS CLOSING BRACE MUST BE PRESENT!
+
 
 # Optional: Associate NSG with a subnet if subnet_id is provided
 resource "azurerm_subnet_network_security_group_association" "this" {
