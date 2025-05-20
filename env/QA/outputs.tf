3f67bf8db993559c565aaad258a2adeeb6f00f16
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 12:55:38 -0400

    Update outputs.tf

--- env/QA/outputs.tf
+++ env/QA/outputs.tf
@@ -1,4 +1,20 @@
-# Remove the duplicate outputs as they are already defined in main.tf
+# Outputs
+output "public_ip_nginx" {
+  value = module.public_ip_nginx.public_ip_address
+}
 
-# You can leave this file empty if outputs are defined in main.tf
+output "key_vault_id" {
+  value = module.key_vault.key_vault_id
+}
 
+output "acr_id" {
+  value = module.acr.acr_id
+}
+
+output "log_analytics_workspace_id" {
+  value = module.log_analytics.workspace_id
+}
+
+output "nginx_ingress_controller_id" {
+  value = helm_release.nginx_ingress.id
+}
