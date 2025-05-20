bb643d76dc692340f14cda5bcada0826aefed6b7
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 18:42:27 -0400

    Update main.tf

--- terraform_modules/terraform-azure-nsg/main.tf
+++ terraform_modules/terraform-azure-nsg/main.tf
@@ -23,8 +23,13 @@ resource "azurerm_network_security_group" "this" {
 
 
 # Optional: Associate NSG with a subnet if subnet_id is provided
-resource "azurerm_subnet_network_security_group_association" "this" {
-  count                     = var.subnet_id != null ? 1 : 0
-  subnet_id                 = var.subnet_id
-  network_security_group_id = azurerm_network_security_group.this.id
+locals {
+  nsg_association = var.subnet_id != null ? { "associate" = var.subnet_id } : {}
 }
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  for_each                   = local.nsg_association
+  subnet_id                  = each.value
+  network_security_group_id  = azurerm_network_security_group.this.id
+}
+
