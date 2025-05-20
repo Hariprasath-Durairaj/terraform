c31276f1e90fedab5aa920bc06f846b9f4e239e0
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 13:33:44 -0400

    Update main.tf

--- terraform_modules/terraform-azure-aks/main.tf
+++ terraform_modules/terraform-azure-aks/main.tf
@@ -36,12 +36,12 @@ resource "azurerm_kubernetes_cluster" "this" {
   private_cluster_enabled          = var.private_cluster_enabled
   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
 
-  addon_profile {
-    oms_agent {
-      enabled                    = var.enable_monitoring
-      log_analytics_workspace_id = var.log_analytics_workspace_id
-    }
+addon_profile {
+  oms_agent {
+    enabled                    = var.enable_monitoring
+    log_analytics_workspace_id = var.log_analytics_workspace_id
   }
+}
 
   tags = var.tags
 }
