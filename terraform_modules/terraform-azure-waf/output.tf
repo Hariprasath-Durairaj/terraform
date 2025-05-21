7ba2a8871b042492d8595c25e9eb7590fa1675da
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 23:33:14 -0400

    Update output.tf

--- terraform_modules/terraform-azure-waf/output.tf
+++ terraform_modules/terraform-azure-waf/output.tf
@@ -4,8 +4,8 @@ output "waf_policy_id" {
 }
 
 output "waf_policy_resource_id" {
-  description = "The resource ID of the WAF policy."
-  value       = azurerm_web_application_firewall_policy.this.resource_id
+  value       = azurerm_web_application_firewall_policy.this.id
+  description = "The resource ID of the WAF policy"
 }
 
 output "waf_policy_name" {
