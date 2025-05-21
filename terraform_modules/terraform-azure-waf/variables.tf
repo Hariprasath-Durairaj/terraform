2bc1619d6e6fe23b99ebec806afe42fda9aaa3a0
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 22:41:37 -0400

    Update variables.tf

--- terraform_modules/terraform-azure-waf/variables.tf
+++ terraform_modules/terraform-azure-waf/variables.tf
@@ -1,38 +1,56 @@
 variable "name" {
-  description = "The name of the WAF policy"
+  description = "The name of the WAF policy."
   type        = string
 }
 
 variable "location" {
-  description = "Azure region"
+  description = "Azure region for the WAF policy."
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Resource group in which to create the WAF policy."
   type        = string
 }
 
 variable "mode" {
-  description = "The mode of the WAF policy (Prevention or Detection)"
+  description = "WAF mode: Prevention or Detection."
   type        = string
   default     = "Prevention"
 }
 
 variable "owasp_version" {
-  description = "The OWASP rule set version"
+  description = "OWASP managed ruleset version (e.g., 3.2)."
   type        = string
   default     = "3.2"
 }
 
+variable "file_upload_limit_in_mb" {
+  description = "File upload limit in MB."
+  type        = number
+  default     = 100
+}
+
+variable "max_request_body_size_in_kb" {
+  description = "Maximum request body size in KB."
+  type        = number
+  default     = 128
+}
+
 variable "custom_rules" {
-  description = "List of custom rules for the WAF policy"
-  type        = list(any)
-  default     = []
+  description = "List of custom WAF rules."
+  type = list(object({
+    name         = string
+    priority     = number
+    rule_type    = string
+    match_values = list(string)
+    action       = string # 'Allow' | 'Block' | 'Log'
+  }))
+  default = []
 }
 
 variable "tags" {
-  description = "Tags to apply to the WAF policy"
+  description = "Tags to apply to the WAF policy."
   type        = map(string)
   default     = {}
 }
