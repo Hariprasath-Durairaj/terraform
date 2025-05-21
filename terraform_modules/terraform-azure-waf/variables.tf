a55b979f5f2a4ba185385e65e0c8eba3ac86f1c5
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-20 23:42:57 -0400

    Update variables.tf

--- terraform_modules/terraform-azure-waf/variables.tf
+++ terraform_modules/terraform-azure-waf/variables.tf
@@ -1,56 +1,39 @@
-variable "name" {
-  description = "The name of the WAF policy."
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region for the WAF policy."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Resource group in which to create the WAF policy."
-  type        = string
-}
-
+variable "name" {}
+variable "location" {}
+variable "resource_group_name" {}
 variable "mode" {
-  description = "WAF mode: Prevention or Detection."
+  description = "WAF policy mode: Prevention or Detection"
   type        = string
   default     = "Prevention"
 }
-
-variable "owasp_version" {
-  description = "OWASP managed ruleset version (e.g., 3.2)."
-  type        = string
-  default     = "3.2"
-}
-
 variable "file_upload_limit_in_mb" {
-  description = "File upload limit in MB."
+  description = "File upload limit in MB"
   type        = number
   default     = 100
 }
-
 variable "max_request_body_size_in_kb" {
-  description = "Maximum request body size in KB."
+  description = "Max request body size in KB"
   type        = number
   default     = 128
 }
-
 variable "custom_rules" {
-  description = "List of custom WAF rules."
-  type = list(object({
+  description = "List of custom rules"
+  type        = list(object({
     name         = string
     priority     = number
     rule_type    = string
     match_values = list(string)
-    action       = string # 'Allow' | 'Block' | 'Log'
+    action       = string
   }))
   default = []
 }
-
+variable "owasp_version" {
+  description = "OWASP managed rule set version"
+  type        = string
+  default     = "3.2"
+}
 variable "tags" {
-  description = "Tags to apply to the WAF policy."
+  description = "Tags for WAF policy"
   type        = map(string)
   default     = {}
 }
