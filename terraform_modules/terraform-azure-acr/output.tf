8d648ea386d6283fe4d2c6c099f0b96199c00b8a
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-05-22 19:11:15 -0400

    Update output.tf

--- terraform_modules/terraform-azure-acr/output.tf
+++ terraform_modules/terraform-azure-acr/output.tf
@@ -1,9 +1,9 @@
 output "acr_id" {
-  description = "The ID of the ACR"
+  description = "The resource ID of the Azure Container Registry"
   value       = azurerm_container_registry.this.id
 }
 
 output "acr_login_server" {
-  description = "The login server URL of the ACR"
+  description = "The login server URL of the Azure Container Registry"
   value       = azurerm_container_registry.this.login_server
 }
